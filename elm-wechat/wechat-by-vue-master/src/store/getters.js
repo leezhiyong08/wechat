6b44ce214992aefb7e6ccebec78459b94db235f5
Lee <lizhiyong_08@163.com>
2019-11-10 21:55:55 +0800

    提交微信聊天项目代码

---
+++ elm-wechat/wechat-by-vue-master/src/store/getters.js
@@ -0,0 +1,4 @@
+//  获取state，并对其做一些映射
+export const info = state => state.info
+
+export const addList = state => state.addList
---
+++ elm-wechat/wechat-by-vue-master/src/store/mutation-types.js
@@ -0,0 +1,3 @@
+// 存放与mutatiom相关的常量
+export const SET_INFO = 'SET_INFO'
+export const SET_ADDLIST = 'SET_ADDLIST'
